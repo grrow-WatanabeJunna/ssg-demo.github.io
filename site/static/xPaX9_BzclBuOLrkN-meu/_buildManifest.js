5a2348c0dd670edbaa9e9bb6883642613d3127f5
grrow-WatanabeJunna <junna.watanabe@grrow.jp>
2022-07-12 15:58:13 +0900

    add site

---
+++ site/static/xPaX9_BzclBuOLrkN-meu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0078c3366e409467.css","static/chunks/pages/index-d4bf8f09070fe874.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
